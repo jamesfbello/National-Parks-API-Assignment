d9c0f2d24de7527236ac59958e0d3bce250514cc
jbello1222 <50038804+jbello1222@users.noreply.github.com>
2019-09-05 22:36:52 -0400

    updated JS to render results in DOM

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 "use strict";
 
+// set constants for API Key and URL endpoint
 const apiKey = "2dlWOGqO0ihu1R0Z5XAoRK8MigE9RosTkcdKRuGX";
 const searchURL = "https://api.nps.gov/api/v1/parks";
 
-//Activate the Form Event Listner
+//set page ready function
 $(document).ready(function() {
   watchSubmitForm();
 });
 
-//Watch the Submit Form Listeners
+//create event listeners for form submission
 function watchSubmitForm() {
   console.log("watchSumbitForm works!");
   $("#search-form").submit(e => {
     e.preventDefault();
     let searchState = $("#state-name-input").val();
     let numResults = $("#number-input").val();
-    getNationalParks(searchState, numResults);
+    getRequestNationalParks(searchState, numResults);
   });
 }
 
-//Format Search Query via Params
+//function to create string to use for URL
 function formatQueryParams(params) {
   console.log("formatQueryParams function works!");
   const queryItems = Object.keys(params).map(
@@ -29,7 +30,7 @@ function formatQueryParams(params) {
 }
 
 //GET Request to National Parks Service API
-function getNationalParks(query, limit = 10) {
+function getRequestNationalParks(query, limit = 10) {
   console.log("getNationalPark works!");
 
   const params = {
@@ -41,7 +42,7 @@ function getNationalParks(query, limit = 10) {
   const queryString = formatQueryParams(params);
   const url = searchURL + "?" + queryString;
 
-  //Test in console whether we get the right search query
+
   console.log(url);
 
   fetch(url)
@@ -57,28 +58,25 @@ function getNationalParks(query, limit = 10) {
     });
 }
 
-//Render GET Request Results to the Dom
+//display request results to DOM
 function displayResults(responseJson) {
   console.log("displayResult function works");
   $("#results-list").empty();
   for (let i = 0; i < responseJson.data.length; i++) {
     $("#results-list").append(`<br> <br>
-    <div class="panel panel-default">
-    <div class="panel-heading">
-      <h3 class="panel-title">${responseJson.data[i].fullName}</h3>
-    </div>
-    <div class="panel-body">
-    <div class= "row>
-     <div class="col-md-3">
-     <h4 class="panel-title">${responseJson.data[i].description}</h4>
-     <p> <p>
-     </div>
-     <div class= "row>
-     <div class="col-md-3">
-     <a href=" ${responseJson.data[i].url}">Visit Park's Website</a>
-     </div>
-    </div> 
-  </div>`);
+    <div class="response-container">
+        <div class="park-name">
+        <h3>${responseJson.data[i].fullName}</h3>
+        </div>
+        <div class="park description">
+        <h4>${responseJson.data[i].description}</h4>
+        <p> <p>
+        </div>
+        <div class="park-website'>
+        <a href="${responseJson.data[i].url}">Visit Park's Website</a>
+        </div>
+        </div> 
+    </div>`);
   }
   $("#results-list").removeClass("hidden");
 }
